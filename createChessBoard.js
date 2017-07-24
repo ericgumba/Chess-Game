eb3c3f50704e48ef971aaeaecfaa68c021c75be7
okmanl <ericgumba@gmail.com>
2017-07-23 23:37:57 -0700

    Created the checkered board.
    Next goal is to create a movable pawn.

--- createChessBoard.js
+++ createChessBoard.js
@@ -1,1 +1,27 @@
+function createBoard() {
 
+    let strHTML = "";
+    let alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+
+    strHTML = '<div id="chessboard">';
+    for (var row = 0; row < 8; row++) {
+        strHTML += "<div>";
+        for (var column = 0; column < 8; column++) {
+
+            if (column == 0) {
+                strHTML += `<div class="white_square" id="${alphabets[column]}${8-row}"></div>`;
+            } else if (column % 2 == 0) {
+                strHTML += `<div class="white_square" id="${alphabets[column]}${8-row}"></div>`;
+            } else {
+                strHTML += `<div class="black_square" id="${alphabets[column]}${8-row}"></div>`;
+            }
+        }
+        strHTML += "</div>";
+    }
+
+    strHTML += "</div>";
+
+
+    document.getElementById('chessboard2').innerHTML = strHTML;
+};
+// createBoard();
