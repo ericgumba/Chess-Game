15551e01b3104201c54534a0a5992e5b4e1b98a6
okmanl <ericgumba@gmail.com>
2017-07-25 17:13:21 -0700

    Well, added the pawns. Have to figure out how to add rules on where it can go...

--- createChessBoard.js
+++ createChessBoard.js
@@ -8,10 +8,42 @@ function createBoard() {
         for (var column = 0; column < 8; column++) {
 
             if ((column + row) % 2 == 0) {
-                strHTML += `<div class="white_square" id="${alphabets[column]}${8-row}"
+                if (row == 1) {
+                    strHTML += `<div class="white_square" id="${alphabets[column]}${8-row}"
+                ondrop="drop(event)" ondragover="allowDrop(event)">
+                <img src="/Users/ericgumba/Chess-Game/images/wP.png" 
+                draggable="true" ondragstart="drag(event)" id="p${row}${column}" width="50" height="50">
+                </div>`;
+
+                } else if (row == 6) {
+                    strHTML += `<div class="white_square" id="${alphabets[column]}${8-row}"
+                ondrop="drop(event)" ondragover="allowDrop(event)">
+                <img src="/Users/ericgumba/Chess-Game/images/bP.png" 
+                draggable="true" ondragstart="drag(event)" id="p${row}${column}" width="50" height="50">
+                </div>`;
+                } else {
+                    strHTML += `<div class="white_square" id="${alphabets[column]}${8-row}"
                 ondrop="drop(event)" ondragover="allowDrop(event)"></div>`;
+                }
             } else {
-                strHTML += `<div class="black_square" id="${alphabets[column]}${8-row}"></div>`;
+
+                if (row == 1) {
+                    strHTML += `<div class="black_square" id="${alphabets[column]}${8-row}"
+                ondrop="drop(event)" ondragover="allowDrop(event)">
+                <img src="/Users/ericgumba/Chess-Game/images/wP.png" 
+                draggable="true" ondragstart="drag(event)" id="p${row}${column}" width="50" height="50">
+                </div>`;
+
+                } else if (row == 6) {
+                    strHTML += `<div class="black_square" id="${alphabets[column]}${8-row}"
+                ondrop="drop(event)" ondragover="allowDrop(event)">
+                <img src="/Users/ericgumba/Chess-Game/images/bP.png" 
+                draggable="true" ondragstart="drag(event)" id="p${row}${column}" width="50" height="50">
+                </div>`;
+                } else {
+                    strHTML += `<div class="black_square" id="${alphabets[column]}${8-row}"
+                ondrop="drop(event)" ondragover="allowDrop(event)"></div>`;
+                }
             }
         }
         strHTML += "</div>";
